653828d98798b19d27f86bb06e23dcabbee74f2f
Lucky <luckyts@gmail.com>
2023-07-25 16:31:58 +0200

    hello world application done

---
+++ src/main/java/lakshmi/App.java
@@ -0,0 +1,13 @@
+package lakshmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
